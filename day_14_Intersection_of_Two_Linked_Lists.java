a9fab2a6c8506e99b3fe3995e0fa8bbd65b1df13
Naman Kumar <112566535+namankr21@users.noreply.github.com>
2023-04-18 19:47:12 +0530

    Day 14 solution.

---
+++ day_14_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,38 @@
+public class day_14_Intersection_of_Two_Linked_Lists {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // boundary check
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        // if a & b have different len, then we will stop the loop after second iteration
+        while (a != b) {
+            // for the end of first iteration, we just reset the pointer to the head of
+            // another linkedlist
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+
+    }
+}

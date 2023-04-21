7e67412801cb44bb45922707a01a41da7535621d
Naman Kumar <112566535+namankr21@users.noreply.github.com>
2023-04-21 22:07:51 +0530

    Day 21 solution.

---
+++ day_21_Integer_to_Roman.java
@@ -0,0 +1,14 @@
+public class day_21_Integer_to_Roman {
+    public static void main(String[] args) {
+      System.out.println(intToRoman(1994));  
+    }
+    public static String intToRoman(int num) {
+        String ans = "";
+        String[] ones= {"","I","II","III","IV","V","VI","VII","VIII","IX"};        
+        String[] tens= {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hundreds= {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thousands= {"","M","MM","MMM"};
+        ans = thousands[num/1000]  + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        return ans;
+    }
+}
